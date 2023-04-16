c9d3e1f3368f68e783f57b0f460099ec84c15834
O's <ed.orantes01@gmail.com>
2023-04-16 10:08:15 -0600

    Datos de la empresa

--- application/views/Base/header.php
+++ application/views/Base/header.php
@@ -261,6 +261,15 @@
 	                     <li class="slide-item"><a href="<?php echo base_url(); ?>Herramientas/movimientos_hojas">Movimientos hoja</a></li>
                      </ul>
                   </li>
+                  
+                  <li class="slide">
+                     <a class="side-menu__item" data-bs-toggle="slide" href="javascript:void(0)"><i  class="side-menu__icon fa fa-building-o"></i><span
+                        class="side-menu__label">Empresa</span><i class="angle fe fe-chevron-right"></i></a>
+                     <ul class="slide-menu">
+                        <li class="side-menu-label1"><a href="javascript:void(0)"></a></li>
+                        <li class="slide-item"><a href="<?php echo base_url(); ?>Empresa/">Información</a></li>
+                     </ul>
+                  </li>
 
             <!--
                   <li>
